3eb7384a3189d4ffd870b138195db2a93a6665d3
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-02-05 21:29:56 -0300

    feat: connecting to the database

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"shortener/src/database"
 	"shortener/src/routers"
 )
 
 func main() {
 	fmt.Println("Link Shortener")
 	routers := routers.Generate()
+	_, err := database.Connect()
+	if err != nil {
+		fmt.Println("Could not start the database")
+	}
+
 	http.ListenAndServe(":8000", routers)
 }
