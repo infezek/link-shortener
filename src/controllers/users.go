c53ddee8a29a17efce88df9ee73b798c40ca08bb
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-02-17 19:48:34 -0300

    feat: generating jwt token on login

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -6,14 +6,52 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"shortener/src/config"
 	"shortener/src/entity"
 	repositories "shortener/src/repository"
+	"shortener/src/security"
+	"time"
+
+	"github.com/golang-jwt/jwt"
 )
 
 func SignIn(db *sql.DB) http.HandlerFunc {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
+			corpoRequisicao, erro := ioutil.ReadAll(r.Body)
+			if erro != nil {
+				return
+			}
 
+			var usuario entity.Users
+			if erro = json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+				return
+			}
+
+			hmacSampleSecret := []byte(config.ProjectSettings().SecretKey)
+
+			repositorios := repositories.UserRepositoryDb{Db: db}
+			valor, err := repositorios.FindByEmail(usuario.Email)
+
+			if err != nil {
+				return
+			}
+			fmt.Println(valor)
+
+			now := time.Now()
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+				"sub":      "123123",
+				"Subject":  "teste",
+				"IssuedAt": now.Unix(),
+				"exp":      now.Add(time.Second * 60).Unix(),
+			})
+			tokenString, err := token.SignedString(hmacSampleSecret)
+
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(tokenString)
+			return
 		},
 	)
 }
@@ -23,20 +61,26 @@ func SignUp(db *sql.DB) http.HandlerFunc {
 		func(w http.ResponseWriter, r *http.Request) {
 			request, _ := ioutil.ReadAll(r.Body)
 
-			var usuario entity.Users
+			var user entity.Users
 
-			if err := json.Unmarshal(request, &usuario); err != nil {
+			if err := json.Unmarshal(request, &user); err != nil {
 				return
 			}
-			if err := usuario.Prepare(); err != nil {
-				return
-			}
-			repositorios := repositories.UserRepositoryDb{Db: db}
-			usuarioID, err := repositorios.Insert(usuario)
+			passwordEncrypt, err := security.EncryptPassword(user.Password)
+			user.Password = passwordEncrypt
 			if err != nil {
 				return
 			}
-			fmt.Println(usuarioID)
+
+			if err := user.Prepare(); err != nil {
+				return
+			}
+			repositorios := repositories.UserRepositoryDb{Db: db}
+			userID, err := repositorios.Insert(user)
+			if err != nil {
+				return
+			}
+			fmt.Println(userID)
 		},
 	)
 }
