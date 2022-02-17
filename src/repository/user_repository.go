0ed52b4628e0ed21ec64dda21d09433c884e8028
Ezequiel Lopes dos Reis Junior <ezequiel.jn98@outlook.com>
2022-02-17 20:06:00 -0300

    feat: looking for the user in the bank by email

--- src/repository/user_repository.go
+++ src/repository/user_repository.go
@@ -28,3 +28,20 @@ func (repo *UserRepositoryDb) Insert(user entity.Users) (int64, error) {
 
 	return id, nil
 }
+
+func (repo *UserRepositoryDb) FindByEmail(email string) (entity.Users, error) {
+	defer repo.Db.Close()
+	fmt.Printf(email)
+	sql_statement := "SELECT * FROM users where email = $1;"
+	userDb, err := repo.Db.Exec(sql_statement, email)
+
+	if err != nil {
+		fmt.Println("err", err)
+		return entity.Users{}, nil
+	}
+	row, _ := userDb.RowsAffected()
+
+	fmt.Println(row)
+
+	return entity.Users{}, nil
+}
